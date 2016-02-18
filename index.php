8552d35251b62b4eba923bdaafabab6f0152993b
HardLev <leonsw@ukr.net>
2016-02-18 17:56:10 +0200

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<?php
-
-echo "Hello!";
-
-?>
+<DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-860">
+	<title> Товарообіг </title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	</head>
+<body>
+ <table cellpadding="0" cellspacing="0" width="860" align="center">
+   <tr>
+     <td colspan="3" class="header"> &nbsp; </td>
+   </tr>
+   <tr>
+     <td class="left_col">
+       <?php
+	     include("menu1.php")
+		?> 
+     </td>
+     <td class="center_col">
+        <p ><span class="head1">Склад товарів</span><br>
+          12 тис. велосипедів, 3,5 тис. пральних засобів, 8 тис. добрив, 
+          700 телевізорів, 1 тис. кухонних приладів     </p>
+		  
+        <p><span class="head1">Робота з клієнтами</span><br>
+          Наша організація забезпечує всіх клієнтів якісним товаром, 
+		    ми завжди піклуємося про наших клієнтів </p>
+			
+      </td>
+    </tr>
+    <tr>
+      <td colspan="3" class="footer"> © ТЦ 2015 </td>
+   </table>
+  </body>
+</html>
